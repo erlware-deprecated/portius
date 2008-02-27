76ccdadd485f742813934506827195c051c1fe7a
Martin Logan <martinjlogan@erlware.org>
2008-02-26 22:29:16 -0600

    added hrl file for shared records

---
+++ lib/portius/include/portius.hrl
@@ -0,0 +1,10 @@
+
+-record(doc_spec, {
+	  transition_id,
+	  webserver_doc_root,
+	  generated_docs_base_dir,
+	  app_index_file_src,
+	  app_index_file,
+	  release_index_file_src,
+	  release_index_file
+	 }).
